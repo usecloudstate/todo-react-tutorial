98f82a673372dcf1e5a63f7cbe41f2e85a1068f0
Nadeesha Cabral <n@nadeesha.me>
2021-11-30 21:57:14 +0000

    Fix default checked state of the input

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -71,7 +71,7 @@ export default function Todo(props) {
           <input
             id={props.id}
             type="checkbox"
-            defaultChecked={props.completed}
+            checked={props.completed}
             onChange={() => props.toggleTaskCompleted(props.id)}
           />
           <label className="todo-label" htmlFor={props.id}>
